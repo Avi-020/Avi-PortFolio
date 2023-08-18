bb1601d4fb1faced9305f6c97ca8e31d87a1720e
Avi-020 <avipawar020@gmail.com>
2023-08-18 17:31:20 +0530

    changes

---
+++ src/data/user.js
@@ -0,0 +1,80 @@
+const INFO = {
+	main: {
+		title: "Portfolio by Avi",
+		name: "Avi Pawar",
+		email: "avipawar020@gmail.com",
+		logo: "../logo1.jpg",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/Avi-020",
+		linkedin: "https://www.linkedin.com/in/avi-pawar-894355271/",
+		instagram: "https://instagram.com/_avi_pawar_020_?igshid=OGQ5ZDc2ODk2ZA==",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer.",
+		description:
+			"I am a Full stack devloper with expertise in Next.js , React.js and MongoDB. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Avi Pawar. I live in Pune, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	
+
+	projects: [
+		{
+			title: "E-commerce website",
+			description:
+				"An e-commerce website is one that allows people to buy and sell physical goods, services, and digital products over the internet rather than at a brick-and-mortar location. ",
+			logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png",
+			linkText: "View Project",
+			link: "https://github.com/Avi-020/shopping_store",
+		},
+
+		{
+			title: "Portfolio website",
+			description:
+				"A portfolio website is a unique way to showcase your work and let others know about yourself.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/Avi-020/Personal_PortFolio",
+		},
+
+		{
+			title: "Voice-To-Text",
+			description:
+				"Voice-To-Text is a reliable and secure web-based speech-to-text tool that enables you to quickly and accurately transcribe your audio and video recordings, as well as dictate your notes instead of typing.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Avi-020/Voice-To-Text",
+		},
+
+		{
+			title: "Daily-News",
+			description:
+				"The world's top news websites encompass several platforms that provide timely and comprehensive coverage of global events, politics, economics, culture, and more. These websites attract millions of visitors daily, shaping public discourse and delivering news to worldwide audiences.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Avi-020/Daily-News",
+		},
+
+		{
+			title: "Text-Editor",
+			description:
+				"Text-Editor is a simple and user-friendly text manipulation tool built with React. It allows you to easily convert text to uppercase, lowercase, and remove spaces and Copy the Text.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
