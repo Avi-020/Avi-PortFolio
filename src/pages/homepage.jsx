e7eb10be72823b61eafe4c0856bd62362dce6ea2
Avi-020 <avipawar020@gmail.com>
2026-03-18 14:46:48 +0530

    add

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -5,7 +5,7 @@ import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-import Logo from "../components/common/logo";
+// import Logo from "../components/common/logo";
 import Footer from "../components/common/footer";
 import NavBar from "../components/common/navBar";
 import AllProjects from "../components/projects/allProjects";
@@ -16,48 +16,48 @@ import SEO from "../data/seo";
 import "./styles/homepage.css";
 
 const Homepage = () => {
-	const [stayLogo, setStayLogo] = useState(false);
-	const [logoSize, setLogoSize] = useState(80);
-	const [oldLogoSize, setOldLogoSize] = useState(80);
+	// const [stayLogo, setStayLogo] = useState(false);
+	// const [logoSize, setLogoSize] = useState(80);
+	// const [oldLogoSize, setOldLogoSize] = useState(80);
 
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
+	// useEffect(() => {
+	// 	window.scrollTo(0, 0);
+	// }, []);
 
-	useEffect(() => {
-		const handleScroll = () => {
-			let scroll = Math.round(window.pageYOffset, 2);
-			let newLogoSize = 80 - (scroll * 4) / 10;
+	// useEffect(() => {
+	// 	const handleScroll = () => {
+	// 		let scroll = Math.round(window.pageYOffset, 2);
+	// 		let newLogoSize = 80 - (scroll * 4) / 10;
 
-			if (newLogoSize < oldLogoSize) {
-				if (newLogoSize > 40) {
-					setLogoSize(newLogoSize);
-					setOldLogoSize(newLogoSize);
-					setStayLogo(false);
-				} else {
-					setStayLogo(true);
-				}
-			} else {
-				setLogoSize(newLogoSize);
-				setStayLogo(false);
-			}
-		};
+	// 		if (newLogoSize < oldLogoSize) {
+	// 			if (newLogoSize > 40) {
+	// 				setLogoSize(newLogoSize);
+	// 				setOldLogoSize(newLogoSize);
+	// 				setStayLogo(false);
+	// 			} else {
+	// 				setStayLogo(true);
+	// 			}
+	// 		} else {
+	// 			setLogoSize(newLogoSize);
+	// 			setStayLogo(false);
+	// 		}
+	// 	};
 
-		window.addEventListener("scroll", handleScroll);
-		return () => window.removeEventListener("scroll", handleScroll);
-	}, [logoSize, oldLogoSize]);
+	// 	window.addEventListener("scroll", handleScroll);
+	// 	return () => window.removeEventListener("scroll", handleScroll);
+	// }, [logoSize, oldLogoSize]);
 
-	const currentSEO = SEO.find((item) => item.page === "home");
+	// const currentSEO = SEO.find((item) => item.page === "home");
 
-	const logoStyle = {
-		display: "flex",
-		position: stayLogo ? "fixed" : "relative",
-		top: stayLogo ? "3vh" : "auto",
-		zIndex: 999,
-		border: stayLogo ? "1px solid white" : "none",
-		borderRadius: stayLogo ? "50%" : "none",
-		boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
-	};
+	// const logoStyle = {
+	// 	display: "flex",
+	// 	position: stayLogo ? "fixed" : "relative",
+	// 	top: stayLogo ? "3vh" : "auto",
+	// 	zIndex: 999,
+	// 	border: stayLogo ? "1px solid white" : "none",
+	// 	borderRadius: stayLogo ? "50%" : "none",
+	// 	boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
+	// };
 
 	return (
 		<>
